70862d0ce6d71d119a612b5f9567c57b2a0095f6
MosaKasem <thescorpion119@gmail.com>
2019-11-21 11:37:04 +0100

    cleaning

--- server/txtToJSON.js
+++ server/txtToJSON.js
@@ -12,12 +12,9 @@ getData(file, 'utf8')
   .then(data => {
     const blogsArr = []
     const blogsObj = {}
-    const cells = data.split('\n') // split by linebreak
-
+    const cells = data.split('\n')
       .map((el) => {
         return el.split('\t')
-        // console.log('el: ', el);
-        // el.split() // DO SOMETHING HERE
       }).map((line, i) => {
         const blogName = line[0].replace(/[^A-Za-z]/g, '')
         blogsArr.push({
@@ -31,27 +28,5 @@ getData(file, 'utf8')
         return line
       })
       console.log(blogsArr[1])
-    // console.log('blogsArr: ', blogsArr)
-
-    // const headings = cells.shift()
-    // console.log(headings)
-    // for(let values of cells) {
-    //     for(let element of headings) {
-    //         console.log('cells:  ', values)
-    //         console.log('headings:  ', element)
-    //     }
-    // }
-
-    // let array = cells.map(el => {
-    //     let obj = {}
-    //     for (let i = 0, l = el.length; i < l; i++) {
-    //       obj[headings[i]] = isNaN(Number(el[i])) ? el[i] : +el[i]
-    //     }
-    //     return obj
-    //   })
-    //   let json = fs.writeFileSync('jsons.json', JSON.stringify(array,  null, '\t'))
-
-    //   console.log('Data: ', array)
-    // console.log(headings)
   })
   .catch(error => console.log('Error: ', error))
