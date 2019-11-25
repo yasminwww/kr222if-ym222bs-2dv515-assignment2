7c3822403023fe3e9671930721b3c32848735b91
MosaKasem <thescorpion119@gmail.com>
2019-11-25 16:11:59 +0100

    m

---
+++ server/algorithms/hcluster.js
@@ -0,0 +1,28 @@
+const blogs = require('../datablog.json')
+const {getRandomRoot, pearson} = require('./pearson')
+
+
+const getCorrelation = () => {
+    const randomRoot = getRandomRoot()
+    const correlationResult = [{currentBlog: randomRoot.blogName}]
+
+    const filteredList = blogs.filter(e => e.blogName !== randomRoot.blogName)
+
+    for (let i = 0; i < filteredList.length; i++) {
+        const currentBlog = filteredList[i];
+        const result = pearson(randomRoot.occurences, currentBlog.occurences)
+        
+        correlationResult.push({blogName: currentBlog.blogName, correlation: result})
+    }
+    // console.log('correlationResult: ', correlationResult)
+    return correlationResult
+}
+getCorrelation()
+
+
+// Nästa steg: 
+
+
+
+
+module.exports.initialize = initialize
