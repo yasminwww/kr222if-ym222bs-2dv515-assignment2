5bdfadb3578da8e9f6e1afca6f6fbff37745c5ed
MosaKasem <thescorpion119@gmail.com>
2019-11-26 20:07:28 +0100

    removing react, agreed it was uncessary

--- server/algorithms/hcluster.js
+++ server/algorithms/hcluster.js
@@ -24,7 +24,6 @@ const clusterifyBlogs = (index) => {
             //  loop through every pair looking for the smallest distance
             
             const result = pearson(rootBlog.occurences, currentBlog.occurences)
-            
 
             correlationResult.push({ 
                 blogName: currentBlog.blogName, 
@@ -41,7 +40,22 @@ const clusterifyBlogs = (index) => {
 }
 
 
-const arrayOfClusters = () => {
+
+
+
+const mergeNodes = (clusterA, clusterB) => {
+        let mergdata = []
+        for (let i = 0; i < clusterA.length; i++) {
+            mergdata.push((clusterA[i] + clusterB[i]) / 2.0);
+        }
+        return mergdata;
+    }
+
+
+
+
+// CODE FROM THE BOOK
+const hierchyBuilder = () => {
     try {
         let clusterArray = []
         let currentClustID = -1
@@ -53,24 +67,50 @@ const arrayOfClusters = () => {
                 id: i
             }))
         }
-        console.log(clusterArray)
     
-        // while (clusterArray.length > 0) {
-        //     let pair = [0, 1]
-        //     let closest =  pearson(clusterArray[0].vec, clusterArray[1].vec)
-            
-        //     for (let i = 0; i < closest.length; i++) {
+        while (clusterArray.length > 1) {
+            let distances = {}
+            let pair = [0, 1]
+            let closest =  pearson(clusterArray[0].vec, clusterArray[1].vec)
 
-        //     }
-        // }
-        return clusterArray
+            
+            for (let i = 0; i < closest.length; i++) {
+                for (let j = 0; j < array.length; j++) {
+                    let currentCluster = clusterArray[i].id + ',' + clusterArray[j].id
+                    if (!(currentCluster in distances)) {
+                        distances[ clusterArray[i].id + ',' + clusterArray[j].id ] = pearson(clusterArray[i].vec, clusterArray[j].vec)
+                    }
+                    let newDistances = distances[i].id + ',' + clusterArray[j].id
+                    if (newDistances < closest) {
+                        closest = d	
+					    pair[0] =i
+					    pair[1] =j
+                    }
+                }
+            }
+            
+		let mergevec = mergeNodes(clusterArray[pair[0]].vec,clusterArray[pair[1]].vec)
+
+
+		let newcluster = new Cluster({vec:mergevec,left:clusterArray[pair[0]],
+										right:clusterArray[pair[1]],
+										distance:closest,id:currentClustID})
+		
+		currentClustID -= 1;
+		
+		clusterArray.splice(pair[1],1)
+		clusterArray.splice(pair[0],1)
+		clusterArray.push(newcluster)
+        }
+
+        return clusterArray[0]
     } catch (err) {
         console.error(err)
     }
 }
-arrayOfClusters()
+hierchyBuilder()
 const findClosest = async () => {
-    const result = arrayOfClusters()
+    const result = hierchyBuilder()
 }
 
 
