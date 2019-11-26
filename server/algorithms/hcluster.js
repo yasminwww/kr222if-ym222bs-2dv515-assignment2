c6acd76ff9cd797898c21793e60440dc2c59fd08
MosaKasem <thescorpion119@gmail.com>
2019-11-26 18:33:01 +0100

    test

--- server/algorithms/hcluster.js
+++ server/algorithms/hcluster.js
@@ -54,7 +54,6 @@ const arrayOfClusters = () => {
             }))
         }
         console.log(clusterArray)
-        
     
         // while (clusterArray.length > 0) {
         //     let pair = [0, 1]
