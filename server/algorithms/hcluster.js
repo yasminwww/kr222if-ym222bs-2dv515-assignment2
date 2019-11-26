f678cecd0c81df1a6331c72f88d53de39625a6a7
MosaKasem <thescorpion119@gmail.com>
2019-11-26 18:27:06 +0100

    progress

--- server/algorithms/hcluster.js
+++ server/algorithms/hcluster.js
@@ -1,40 +1,78 @@
 const blogs = require('../datablog.json')
-const {getRandomRoot, pearson} = require('./pearson')
-const Cluster = require('../model/cluster')
+const { getRandomRoot, pearson } = require('./pearson')
+const Cluster = require('../model/Cluster')
+const Blog = require('../model/blog')
 
+/**
+ * 
+ * @param {number} index, array contains other arrays, every array holds (blog name) and (word occurences)
+ * blogname: ____
+ * occurences: [3, 5, 20, etc...]
+ */
+const clusterifyBlogs = (index) => {
+    try {
+        const rootBlog = blogs[index]
+        const correlationResult = [{ 
+            rootBlogName: rootBlog.blogName, 
+            occurences: rootBlog.occurences
+        }]
 
-const getCorrelation = (index) => {
-    const rootBlog = blogs[index]
-    const correlationResult = [{rootBlogName: rootBlog.blogName, occurences: rootBlog.occurences}]
+        const filteredList = blogs.filter(e => e.blogName !== rootBlog.blogName)
 
-    const filteredList = blogs.filter(e => e.blogName !== rootBlog.blogName)
+        for (let i = 0; i < filteredList.length; i++) {
+            const currentBlog = filteredList[i]
+            //  loop through every pair looking for the smallest distance
+            
+            const result = pearson(rootBlog.occurences, currentBlog.occurences)
+            
 
-    for (let i = 0; i < filteredList.length; i++) {
-        const currentBlog = filteredList[i];
-        const result = pearson(rootBlog.occurences, currentBlog.occurences)
+            correlationResult.push({ 
+                blogName: currentBlog.blogName, 
+                occurences: currentBlog.occurences, 
+                correlation: result 
+            })
+        }
         
-        correlationResult.push({blogName: currentBlog.blogName, occurences: currentBlog.occurences , correlation: result})
+        return correlationResult
+
+    } catch (err) {
+        console.error(err)
     }
-    return correlationResult
-    
 }
 
 
-// Nästa steg: 
-const merging = () => {
-    let arrayOfClusters = []
+const arrayOfClusters = () => {
+    try {
+        let clusterArray = []
+        let currentClustID = -1
 
-    for (let i = 0; i < blogs.length; i++) {
-        const result = getCorrelation(i);
-        // 
-        console.log('result: ', result);
-    }
-    const res = getCorrelation()
+        for (let i = 0; i < blogs.length; i++) {
+            clusterArray.push(new Cluster({
+                blog:blogs[i].blogName,
+                vec: blogs[i].occurences,
+                id: i
+            }))
+        }
+        console.log(clusterArray)
     
+        // while (clusterArray.length > 0) {
+        //     let pair = [0, 1]
+        //     let closest =  pearson(clusterArray[0].vec, clusterArray[1].vec)
+            
+        //     for (let i = 0; i < closest.length; i++) {
 
+        //     }
+        // }
+        return clusterArray
+    } catch (err) {
+        console.error(err)
+    }
 }
-merging()
+arrayOfClusters()
+const findClosest = async () => {
+    const result = arrayOfClusters()
+}
 
 
-module.exports.getCorrelation = getCorrelation
+module.exports.getCorrelation = clusterifyBlogs
 
--- server/model/cluster.js
+++ server/model/cluster.js
@@ -1,9 +1,11 @@
 class Cluster {
-    constructor(left, right, vec, distance, blog) {
-        this.left = left || null
-        this.right = right || null
-        this.vec = vec
-        this.distance = distance || 0
-        this.blog = blog
+    constructor(opt) {
+        this.left = opt.left || null
+        this.right = opt.right || null
+        this.vec = opt.vec
+        this.distance = opt.distance || 0
+        this.blog = opt.blog
+        this.id = opt.id
     }
 }
+module.exports = Cluster
