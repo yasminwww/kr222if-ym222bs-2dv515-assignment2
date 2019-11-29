bd78fa84b30246ccdff90158ecc4556e920e4bca
MosaKasem <thescorpion119@gmail.com>
2019-11-29 09:53:51 +0100

    blognames rendered

--- client/src/components/RenderTree.js
+++ client/src/components/RenderTree.js
@@ -7,20 +7,52 @@ const RenderTree = () => {
     const fetchData = async () => {
         try {
             const response = await axios.get('/kmeans')
-            console.log(response.data)
-            setData(response.data)
+            // console.log(response.data)
+            kMeans(response.data)
+            // setData(response.data)
         } catch(err) {
             console.log(err)
         }
     }
 
+    const kMeans = (clusters) => {
+        const centroids = []
+        clusters.map((cluster, i) => {
+            centroids.push({cluster: []})
+            cluster.assignments.map((blog) => {
+                centroids[i].cluster.push(blog)
+            })
+        })
+        setData(centroids)
+        // data.map((data) => {
+        //     console.log(data)
+        // })
+    }
+    // data.map( (clusters, i) => (
+    //     <li key={`${clusters.name} ${i}`}>
+    //         cluster {i}
+    //         {clusters && clusters.map((blogs, i) => {
+    //            <div></div>  
+    // ))
+    //     </li>
+    // ))
+
     return (
         <div className='jstree'>
         { data &&
-                    data.map( (data, i) => (
-                        <li key={`${data.name} ${i}`}>
-                        </li>
+            data.map((cluster, i) => (
+                // console.log('cluster[i]: ', data[i].cluster.map((e) => (console.log(e.blogName))))
+                // console.log('cluster[i]: ', data[i].cluster[0])
+                // cluster[i]
+                <li key={`${cluster.name} ${i}`}>
+                    cluster: {i}
+                    {data[i].cluster && data[i].cluster.map((blog, index) => (
+                        <p key={`${blog.blogName} ${index}`}>{blog.blogName}</p>
                     ))
+                    }
+                    <br></br>
+                </li>
+            ))
         }
         <button type="button" className=" p-3 ml-0 btn btn-warning" onClick={() => fetchData()}>demo button</button>
         </div>
@@ -29,66 +61,3 @@ const RenderTree = () => {
 
 
 export default RenderTree
-
-// import React, {Component} from 'react';
- 
-// export class Tree extends Component {
- 
-//   constructor(props) {
-//     super(props);
-//     this.state = {
-//       selectNode: ['anyNode'],
-//       data: {
-//         core: {
-//           data: [
-//             {
-//               text: 'Root node', children: [
-//               {text: 'Child node 1'},
-//               {text: 'Child node 2'}
-//               ]
-//             }
-//           ]
-//         }
-//       },
-//       selected: [],
-//     };
-//   }
- 
-//   async handleClick() {
-//     const w = await axios.get('/kmeans')
-// console.log(w);
-//     const newData = this.state.data.core.data[0].children.slice();
-//     newData.push({text: 'New child node'});
-//     this.setState({
-//       data: {
-//         core: {
-//           data: [
-//               ...w.data
-//           ]
-//         }
-//       }
-//     });
-//   }
- 
-//   handleChange(e, data) {
-//     this.setState({
-//       selected: data.selected,
-//     })
-//   }
- 
-//   render() {
-//     const data = this.state.data;
- 
-//     return (
-//       <div>
-//         <button onClick={() => this.handleClick()}>Add node</button>
-//         <br/><br/>
-//         <TreeView treeData={data} selectNode={this.state.selectNode} onChange={(e, data) => this.handleChange(e, data)} />
-//         <br />
-//         <p>Selected nodes: {this.state.selected.join(', ')}</p>
-//       </div>
-//     );
-//   }
-// }
-
-// export default Tree
